15f78eb0acda5dd749092802fca0d26f75a79faf
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-18 15:01:52 +0200

    modded

--- VND_VP_EDUBE/PYTHON/python sample file.py
+++ VND_VP_EDUBE/PYTHON/python sample file.py
@@ -8,3 +8,13 @@ print("Good Bye")
 
 print("The itsy bitsy spider climbed up the waterspout.")
 print("Down came the rain and washed the spider out.")
+
+# HOW TO CREATE SPACE INBETWEEN
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain and washed the spider out.")
+
+# FOR BETTER NEWLINING WE USE BACKSLASH & N (\n)
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
