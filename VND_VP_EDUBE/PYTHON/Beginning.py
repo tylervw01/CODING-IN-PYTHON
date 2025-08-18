c1191c7323ab46ec8ce4a455e9ced7b8e93ded0e
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-18 15:26:59 +0200

    new + mod

--- VND_VP_EDUBE/PYTHON/Beginning.py
+++ VND_VP_EDUBE/PYTHON/Beginning.py
@@ -15,6 +15,9 @@ print("The itsy bitsy spider\nclimbed up the waterspout.")
 print()
 print("Down came the rain\nand washed the spider out.")
 
-# END KEYWORD
+# END KEYWORD ARGUMENTS - PULLS THE BOTTOM TO THE TOP
 print("My name is", "Python.", end=" ")
 print("Monty Python.")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
