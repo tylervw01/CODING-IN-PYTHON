8fcd9c683182b92f00c7dcb95ee87f8f9893e82b
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-19 08:55:40 +0200

    modded

--- VND_VP_EDUBE/LABS/LAB[2.1.1.19].py
+++ VND_VP_EDUBE/LABS/LAB[2.1.1.19].py
@@ -1,15 +1,15 @@
-print("      *")
-print("     * *")
-print("    *   *")
-print("   *     *")
-print("  *       *")
-print(" ***     ***")
-print("   *    *")
-print("   *    *")
-print("   *    *")
-print("   *    *")
-print("   *    *") 
-print("   ******") 
+# print("      *")
+# print("     * *")
+# print("    *   *")
+# print("   *     *")
+# print("  *       *")
+# print(" ***     ***")
+# print("   *    *")
+# print("   *    *")
+# print("   *    *")
+# print("   *    *")
+# print("   *    *") 
+# print("   ******") 
 #######################################
 # print("************")
 # print("     **")
@@ -39,4 +39,17 @@ print("   ******")
 ###############################################
 # MODDED VERSION:
 ###############################################
-print("             *", "\n            * *", "\n        *  *", "\n  *   *", '\n**')
+# print("             *", "\n            * *", "\n        *  *", "\n  *   *", '\n**')
+# Define the arrow shape with double size and proportions
+arrow = (
+    "    * *\n"
+    "   *    *\n"  
+    "**************"  
+)
+
+# # Print the arrow once
+# print("Single Arrow:\n" + arrow)
+
+# # Print the arrow twice side by side
+# print("\nDouble Arrow Side by Side:")
+# print(arrow + "  " + arrow)
