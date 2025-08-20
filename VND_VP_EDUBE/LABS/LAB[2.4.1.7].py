837828fa2b58a039c9d298d47a84e119b7fab728
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-20 13:06:00 +0200

    new file + rename

---
+++ VND_VP_EDUBE/LABS/LAB[2.4.1.7].py
@@ -0,0 +1,10 @@
+var = 1
+account_balance = 95250.0
+client_name = 'Tyler Van Wyk'
+print(var, account_balance, client_name)
+print(var)
+
+# a = 3.0
+# b = 4.0
+# c = (a ** 2 + b ** 2) ** 0.5
+# print("c =", c)
