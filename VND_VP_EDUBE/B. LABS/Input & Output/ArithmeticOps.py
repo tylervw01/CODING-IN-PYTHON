c2c77eda068c4ed62764066c815f0183eaa5ed94
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-20 15:31:07 +0200

    modded

--- VND_VP_EDUBE/B. LABS/Input & Output/ArithmeticOps.py
+++ VND_VP_EDUBE/B. LABS/Input & Output/ArithmeticOps.py
@@ -1,8 +1,11 @@
+#SCENARIO:
+#My task is to complete the code in order to evaluate the 
+#results of four basic arithmetic operations.
 var1 = 25.5
 var2 = 34.4 
 # output the result of addition here
 Varible_In_Addition = var1 + var2
-print("Added Output= ", Varible_In_Addition)
+print(Varible_In_Addition, )
 
 # output the result of subtraction here
 Variable_In_Subtraction = var1 - var2
