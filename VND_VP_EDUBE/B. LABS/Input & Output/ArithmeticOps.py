fb8c614f016296cd790341485e8bbdd6446aa846
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-20 15:19:53 +0200

    modded

--- VND_VP_EDUBE/B. LABS/Input & Output/ArithmeticOps.py
+++ VND_VP_EDUBE/B. LABS/Input & Output/ArithmeticOps.py
@@ -1,9 +1,19 @@
-# input a float value for variable a here
-# input a float value for variable b here
-
+var1 = 25.5
+var2 = 34.4 
 # output the result of addition here
+Varible_In_Addition = var1 + var2
+print("Added Output= ", Varible_In_Addition)
+
 # output the result of subtraction here
+Variable_In_Subtraction = var1 - var2
+print("Subtracted Output= ", Variable_In_Subtraction)
+
 # output the result of multiplication here
+Variable_In_Multiplication = var1 * var2
+print("Multiplied Output= ", Variable_In_Multiplication)
+
 # output the result of division here
+Variable_In_Division = var1 / var2
+print("Divided Output= ", Variable_In_Division)
 
 print("\nThat's all, folks!")
