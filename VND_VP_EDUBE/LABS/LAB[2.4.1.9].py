df0cbbdeb23c10f9af3d32a7732d24b946b992b7
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-20 13:45:36 +0200

    mod

--- VND_VP_EDUBE/LABS/LAB[2.4.1.9].py
+++ VND_VP_EDUBE/LABS/LAB[2.4.1.9].py
@@ -4,11 +4,10 @@
 # experimenting with Python code.
 # Scenario
 # Miles and kilometers are units of length or distance.
-Kilometers = 12.25
 Miles = 7.38
-
-Miles_To_Kilometers = Miles * 1.60934 
-Kilometers_To_Miles = Kilometers / 1.60934
+Kilometers = 12.25
+Miles_To_Kilometers = Miles * 1.61
+Kilometers_To_Miles = Kilometers / 1.61
 
 print(Miles, "MILES IS", round(Miles_To_Kilometers, 2), "KILOMETERS")
 print(Kilometers, "KILOMETERS IS", round(Kilometers_To_Miles, 2), "MILES")
