d919d249f672593387e510b9695ff9f9917343e1
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-20 14:30:29 +0200

    mostly renames + 1 new file

---
+++ VND_VP_EDUBE/2 LABS/Comments/LAB[2.5.1.2].py
@@ -0,0 +1,21 @@
+# Objectives:
+# becoming familiar with the concept of comments in Python;
+# using and not using comments;
+# replacing Comments with code;
+#     SCENARIO:
+# The code in the editor contains comments. Try to improve it: add or remove comments
+#  where you find it appropriate( yes, sometimes removing a comment can make the code more readalbe), and change variable names where you think this will imporve code comprehension.
+##################
+
+#this program computes the number of seconds in a given number of hours
+# this program has been written two days ago
+
+a = 4 # number of hours 
+seconds = 6300 # number of seconds in hour
+
+print("Hours: ", a) #printing the number of hours
+print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+
+#here we should also print "Goodbye", but a programmer didn't have time to write any code
+print("GoodBye")
+# this is the end of the program that computes the number of seconds
