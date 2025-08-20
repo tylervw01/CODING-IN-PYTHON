c50eacb789c8bf5404f7e6f8a5c7987d58a486ab
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-20 13:38:07 +0200

    new LAB.9: a simple converter

---
+++ VND_VP_EDUBE/LABS/LAB[2.4.1.9].py
@@ -0,0 +1,8 @@
+Kilometers = 12.25
+Miles = 7.38
+
+Miles_To_Kilometers = Miles * 1.60934 
+Kilometers_To_Miles = Kilometers / 1.60934
+
+print(Miles, "miles is", round(Miles_To_Kilometers, 2), "kilometers")
+print(Kilometers, "kilometers is", round(Kilometers_To_Miles, 2), "miles")
