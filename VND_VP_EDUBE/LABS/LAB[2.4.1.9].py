17c0017c07eb241a8670f21f973008dffda70250
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-20 13:39:50 +0200

    MOD

--- VND_VP_EDUBE/LABS/LAB[2.4.1.9].py
+++ VND_VP_EDUBE/LABS/LAB[2.4.1.9].py
@@ -4,5 +4,5 @@ Miles = 7.38
 Miles_To_Kilometers = Miles * 1.60934 
 Kilometers_To_Miles = Kilometers / 1.60934
 
-print(Miles, "miles is", round(Miles_To_Kilometers, 2), "kilometers")
-print(Kilometers, "kilometers is", round(Kilometers_To_Miles, 2), "miles")
+print(Miles, "MILES IS", round(Miles_To_Kilometers, 2), "KILOMETERS")
+print(Kilometers, "KILOMETERS IS", round(Kilometers_To_Miles, 2), "MILES")
