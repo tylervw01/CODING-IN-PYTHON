c76f72a86819cdc16358225e6f1494712e698117
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-20 21:21:05 +0200

    new file

---
+++ OUT'SOURCE/LESSON 1/Hello.py
@@ -0,0 +1,12 @@
+# greeting = 'Good Morning'
+# print(greeting)
+print1 = "******************************************"
+print2 = "*                                        *"
+print3 = "*                                        *"
+print3 = "*             WELCOME MAN!               *"
+
+print(print1)
+print(print2)
+print(print3)
+print(print2)
+print(print1)
