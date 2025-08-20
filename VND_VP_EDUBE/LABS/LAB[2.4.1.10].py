428a5335abb0df0c996a49a152936bd2a8c76626
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-20 14:07:33 +0200

    modded

--- VND_VP_EDUBE/LABS/LAB[2.4.1.10].py
+++ VND_VP_EDUBE/LABS/LAB[2.4.1.10].py
@@ -4,12 +4,8 @@
 # Scenario
 # Take a look at the code in the editor: it reads a float value, puts it into a variable named x, and prints the value of a variable named y. Your task is to complete the code in order to evaluate the following expression:
 # 3x3 - 2x2 + 3x - 1
-# x = input("10: ")
-###########################################
-# Start Code below this line!
-# ###########################################
-x = float(10.)
-y =
+##########################################################################
 
-y = x ** 2
-print("y =", y)
+x = float(input())
+y = 3 * x * x * x - 2 * x * x + 3 * x - 1
+print(y)
