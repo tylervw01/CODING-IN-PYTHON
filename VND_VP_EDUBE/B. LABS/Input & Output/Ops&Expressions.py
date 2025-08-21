ced5204648b242bc67c41096971cb2042ccb5031
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-21 08:56:13 +0200

    expression understood

--- VND_VP_EDUBE/B. LABS/Input & Output/Ops&Expressions.py
+++ VND_VP_EDUBE/B. LABS/Input & Output/Ops&Expressions.py
@@ -1,7 +1,7 @@
 x = float(input("Enter value for x: "))
 # y = float(input("Enter value for y: "))
 
-# Write your code here.
+# Write your code here. FOLLOWING EXPRESSION
 y = 1 / (x + 1/(x + 1 /(x + (1 / x))))
 
 # x = y * 2
