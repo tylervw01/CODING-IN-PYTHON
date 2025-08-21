c3f0ef0aa41336aa76fb34eeeaec3959b4302373
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-21 08:59:33 +0200

    mod

--- VND_VP_EDUBE/B. LABS/Input & Output/Ops&Expressions.py
+++ VND_VP_EDUBE/B. LABS/Input & Output/Ops&Expressions.py
@@ -1,10 +1,6 @@
 x = float(input("Enter value for x: "))
-# y = float(input("Enter value for y: "))
 
 # Write your code here. FOLLOWING EXPRESSION
 y = 1 / (x + 1/(x + 1 /(x + (1 / x))))
 
-# x = y * 2
-
 print("y =", y,)
-# print("x =", x)
