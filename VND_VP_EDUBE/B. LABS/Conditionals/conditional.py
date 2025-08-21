1b30147d49bb7abbbd656103fcf3559c30a9640d
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-21 10:08:51 +0200

    new + mod

---
+++ VND_VP_EDUBE/B. LABS/Conditionals/conditional.py
@@ -0,0 +1,15 @@
+# Inputs for the largest pant
+Spathiphyllum = (input("Enter the first flower: "))
+spathiphyllum = (input("Enter the second flower: "))
+
+# We now declare who is bigger
+largest_flower = Spathiphyllum
+
+# The If statement
+if spathiphyllum < largest_flower:
+    largest_flower = spathiphyllum
+
+# Print the result
+print("Yes, Spathiphyllum is the best plant:", largest_flower)
+print( )
+ 
--- VND_VP_EDUBE/B. LABS/Decisions/Qs&As.py
+++ VND_VP_EDUBE/B. LABS/Decisions/Qs&As.py
@@ -1,8 +1,6 @@
+value1 = int(print("Enter value: "))
 
 var = 100 
 print(var == 100)
 var = 100 
 print(var == 50)
-
-
-
