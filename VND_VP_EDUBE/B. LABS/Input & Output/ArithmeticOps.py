bde3e3729d8ae71ca86319a3690c8f3f385fc51c
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-21 06:33:27 +0200

    mod

--- VND_VP_EDUBE/B. LABS/Input & Output/ArithmeticOps.py
+++ VND_VP_EDUBE/B. LABS/Input & Output/ArithmeticOps.py
@@ -1,22 +1,27 @@
 #SCENARIO:
 #My task is to complete the code in order to evaluate the 
 #results of four basic arithmetic operations.
-var1 = 25.5
-var2 = 34.4 
-# output the result of addition here
-Varible_In_Addition = var1 + var2
-print(Varible_In_Addition, )
+#######################
+# Input float values from the user
 
-# output the result of subtraction here
-Variable_In_Subtraction = var1 - var2
-print("Subtracted Output= ", Variable_In_Subtraction)
+var1 = float(input("Enter the first number (var1): "))
+var2 = float(input("Enter the second number (var2): "))
 
-# output the result of multiplication here
-Variable_In_Multiplication = var1 * var2
-print("Multiplied Output= ", Variable_In_Multiplication)
 
-# output the result of division here
-Variable_In_Division = var1 / var2
-print("Divided Output= ", Variable_In_Division)
+# Output the result of addition
+result_addition = var1 + var2
+print("Addition Result =", result_addition)
+
+# Output the result of subtraction
+result_subtraction = var1 - var2
+print("Subtraction Result =", result_subtraction)
+
+# Output the result of multiplication
+result_multiplication = var1 * var2
+print("Multiplication Result =", result_multiplication)
+
+# Output the result of division
+result_division = var1 / var2
+print("Division Result =", result_division)
 
 print("\nThat's all, folks!")
