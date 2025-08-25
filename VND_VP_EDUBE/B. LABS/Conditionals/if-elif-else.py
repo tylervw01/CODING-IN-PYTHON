9e35587957ef58ed884f814062954976f2537a83
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-25 22:02:41 +0200

    mod

--- VND_VP_EDUBE/B. LABS/Conditionals/if-elif-else.py
+++ VND_VP_EDUBE/B. LABS/Conditionals/if-elif-else.py
@@ -3,3 +3,12 @@ year = int (input("Enter a year: "))
 if year < 1582:
     print("Not in Calander")
 else:
+    if year % 4 != 0:
+        print("Common Year")
+    elif year % 100 != 0:
+        print("Leap Year")
+    elif year % 400 != 0:
+        print("Common Year")
+    else:
+        print("Leap Year")
+        
