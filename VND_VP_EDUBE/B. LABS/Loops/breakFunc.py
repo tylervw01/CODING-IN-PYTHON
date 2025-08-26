f589bc8fb1bbabf33021180932b840cc268893e2
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-26 09:39:28 +0200

    moded

--- VND_VP_EDUBE/B. LABS/Loops/breakFunc.py
+++ VND_VP_EDUBE/B. LABS/Loops/breakFunc.py
@@ -1,5 +1,5 @@
-# while True:
-#     word = input("Enter a word: ")
-#     if word == "chupacabra":
-#         print("You've successfully left the loop.")
-#         break
+while True:
+    word = input("Enter a word: ")
+    if word == "chupacabra":
+        print("You've successfully left the loop.")
+        break
