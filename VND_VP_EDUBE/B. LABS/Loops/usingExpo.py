36b7bff7c38b757d54a5d4537c226220a0d57d92
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-26 09:13:22 +0200

    modded

--- VND_VP_EDUBE/B. LABS/Loops/usingExpo.py
+++ VND_VP_EDUBE/B. LABS/Loops/usingExpo.py
@@ -1,7 +1,14 @@
+# power = 1
+# for expo in range(16):
+#     print("2 to the power of", expo, "is", power)
+#     power *= 2
+
 import time
 
 for i in range(1, 6):
-    print(f"{i} Mississippi")
+    print("Mississippi")
+    # print(f"{i} Mississippi")
     time.sleep(1)
 
 print("Ready or not, here I come!")
+
