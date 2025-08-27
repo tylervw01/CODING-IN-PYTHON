c20e299213d95ed5c53c293cf89f9c4aca72ab23
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-08-27 15:04:54 +0200

    modded

---
+++ VND_VP_EDUBE/B. LABS/Lists/lists.py
@@ -0,0 +1,25 @@
+# hat_list = [1, 2, 3, 4, 5]
+
+# hat_list[2] = int(input("Enter a number to replace the middle value: "))
+# print("New value added: ", hat_list)
+
+# hat_list.pop()
+
+# del hat_list[-1]
+# print("Deleted value: ",hat_list)
+# print(len(hat_list))
+# print("The value you have entered", hat_list)
+
+##########################################################
+
+hat_list = [1, 2, 3, 4, 5]
+
+# Step 1
+hat_list[2] = int(input("Enter an integer number: "))
+
+# Step 2
+del hat_list[-1]
+print("deleted value: ",hat_list)
+
+# Step 3
+print(len(hat_list))
