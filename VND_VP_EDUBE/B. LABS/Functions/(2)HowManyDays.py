9fb38256336a563c474da91eba6ec308295f67e9
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-01 15:29:58 +0200

    new new

---
+++ VND_VP_EDUBE/B. LABS/Functions/(2)HowManyDays.py
@@ -0,0 +1,40 @@
+#SCENARIO:
+# -Your task is to write and test a function which takes two arguements(a year and a month) and returns the number of days for the given month/year pair (while only February) is sensitive to the YEAR value, your function should be universal).
+# #
+def is_year_leap(year):
+    #code from lab 6
+    def days_in_month(year, month):
+        #new code here
+        #
+    
+    def is_year_leap(year):
+	if year % 4 != 0:
+		return False
+	elif year % 100 != 0:
+		return True
+	elif year % 400 != 0:
+		return False
+	else:
+		return True
+
+def days_in_month(year,month):
+	if year < 1582 or month < 1 or month > 12:
+		return None
+	days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	res  = days[month - 1]
+	if month == 2 and is_year_leap(year):
+		res = 29
+	return res
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [ 2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+	yr = test_years[i]
+	mo = test_months[i]
+	print(yr,mo,"-> ",end="")
+	result = days_in_month(yr, mo)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
