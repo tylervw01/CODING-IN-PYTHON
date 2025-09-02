b3a5b00e4e89924461f6ca396f276315ceaba0ac
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-02 10:35:23 +0200

    new F + modify

---
+++ VND_VP_EDUBE/A. PYTHON NOTES/4.5. Creating Functions/Factorial.py
@@ -0,0 +1,28 @@
+# ###
+# #
+# Some simple functions: factorials
+# Another function we're about to write is factorials. Do you remember how a factorial is defined?
+
+# 0! = 1 (yes! it's true)
+# 1! = 1
+# 2! = 1 * 2
+# 3! = 1 * 2 * 3
+# 4! = 1 * 2 * 3 * 4
+# :
+# :
+# n! = 1 * 2 * 3 * 4 * ... * n-1 * n
+def factorial_function(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    
+    product = 1
+    for i in range(2, n + 1):
+        product *= i
+    return product
+
+
+for n in range(1, 6):  # testing
+    print(n, factorial_function(n))
+
--- VND_VP_EDUBE/A. PYTHON NOTES/4.5. Creating Functions/TestingTriangles.py
+++ VND_VP_EDUBE/A. PYTHON NOTES/4.5. Creating Functions/TestingTriangles.py
@@ -14,3 +14,34 @@ def is_a_right_triangle(a, b, c):
 print(is_a_right_triangle(5, 3, 4))
 print(is_a_right_triangle(1, 3, 4))
 
+#######################################
+#RIGHT-ANGLE TRIANGLE
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+def heron(a, b, c):
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+
+def area_of_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
+        return None
+    return heron(a, b, c)
+
+
+print(area_of_triangle(1., 1., 2. ** .5))
+############
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+a = float(input('Enter the first side\'s length: '))
+b = float(input('Enter the second side\'s length: '))
+c = float(input('Enter the third side\'s length: '))
+
+if is_a_triangle(a, b, c):
+    print('Yes, it can be a triangle.')
+else:
+    print('No, it can\'t be a triangle.')
