5f9408f22a324d18362bf0b65aa00cd5ffe4fcd5
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-03 15:30:14 +0200

    modified:   VND_VP_EDUBE/MOCK/Mock tests 2/Question2.py

--- VND_VP_EDUBE/MOCK/Mock tests 2/Question2.py
+++ VND_VP_EDUBE/MOCK/Mock tests 2/Question2.py
@@ -1,8 +1,2 @@
 # Question 2
 #Write a prgram that:
-scores = {"Alice": 85, "Bob": 90, "Charlie": 78}
-print("Keys:", list(scores.keys()))
-print("Values:", list(scores.values()))
-scores["Bob"] = 95
-scores["Diana"] = 88
-print("Updated dictionary:", scores)
