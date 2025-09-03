22b1504c1f566222a45d418a00c5097b1e9a0cb7
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-03 13:28:34 +0200

    module 4.6

---
+++ VND_VP_EDUBE/A. PYTHON NOTES/4.6. Tuples & Dictionaries/TheKeys().py
@@ -0,0 +1,13 @@
+# dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+
+# for key in dictionary.keys():
+#     print(key, "->", dictionary[key]
+
+# cat -> chat
+# dog -> chien
+# horse -> cheval
+######################
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+
+for key in dictionary.keys():
+    print(key, "->", dictionary[key])
