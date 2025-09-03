97ae81e5831d75ab3000eef96d887d0cfefcd737
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-03 18:29:14 +0200

    modded

--- VND_VP_EDUBE/MOCK/Mock tests 2/Question1.py
+++ VND_VP_EDUBE/MOCK/Mock tests 2/Question1.py
@@ -4,4 +4,4 @@ my_tuple = ("apple", "banana", "cherry", "apple", "orange", "banana")
 
 print("Length of tuple:", len(my_tuple))
 print("Number of times 'apple' appears:", my_tuple.count("apple"))
-print("Index of first Occurrence of 'Cherry': ", my_tuple.index("cherry"))
+print("Index of first Occurrence of 'cherry': ", my_tuple.index("cherry"))
