8eb2da5cb5c79826775fc557fdc40312480c4e0c
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-03 15:10:56 +0200

    modded

---
+++ VND_VP_EDUBE/MOCK/Mock tests 2/Question1.py
@@ -0,0 +1,18 @@
+#Question 1:
+#Writting a python program that Creates, prints length, prints apples & finds and prints the index
+
+# Question 1
+# fruits = ("apple", "banana", "cherry", "apple", "orange", "banana")
+# print("Length of tuple:", len(fruits))
+# print("Number of times 'apple' appears:", fruits.count("apple"))
+# print("Index of first occurrence of 'cherry':", fruits.index("cherry"))
+
+
+
+
+
+
+my_tuple = ("apple","banana","cherry","apple","orange","banana")
+print(len(my_tuple))
+print(index(my_tuple[apple]))
+print()
