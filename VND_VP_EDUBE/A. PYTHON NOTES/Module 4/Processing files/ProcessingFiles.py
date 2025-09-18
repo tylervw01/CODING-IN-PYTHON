27cd4945f87ad0cafd82cc524a767685ceef5622
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-18 09:27:03 +0200

    modified:   VND_VP_EDUBE/A. PYTHON NOTES/Module 4/Processing files/ProcessingFiles.py

--- VND_VP_EDUBE/A. PYTHON NOTES/Module 4/Processing files/ProcessingFiles.py
+++ VND_VP_EDUBE/A. PYTHON NOTES/Module 4/Processing files/ProcessingFiles.py
@@ -1,16 +1,16 @@
-import errno
+# import errno
 
-try:
-    s = open("C:\\Users\\Tyler\\OneDrive - NewBridge Graduate Institute\\Desktop\\file.txt", "rt")
-    # Actual processing goes here.
-    s.close()
-except Exception as exc:
-    if exc.errno == errno.ENOENT:
-        print("The file doesn't exist.")
-    elif exc.errno == errno.EMFILE:
-        print("You've opened too many files.")
-    else:
-        print("The error number is:", exc.errno)
+# try:
+#     s = open("C:\Users\Tyler\OneDrive - NewBridge Graduate Institute\Desktop\file.txt", "rt")
+#     # Actual processing goes here.
+#     s.close()
+# except Exception as exc:
+#     if exc.errno == errno.ENOENT:
+#         print("The file doesn't exist.")
+#     elif exc.errno == errno.EMFILE:
+#         print("You've opened too many files.")
+#     else:
+#         print("The error number is:", exc.errno)
 
 # try:
 #     stream = open("C:\Users\User\Desktop\\file.txt", "rt")
@@ -19,3 +19,7 @@ except Exception as exc:
 # except Exception as exc:
 #     print("Cannot open the file:", exc)
 
+# Opening tzop.txt in read mode, returning it as a file object:
+# stream = open("tzop.txt", "rt", encoding = "utf-8")
+
+# print(stream.read()) # printing the content of the file
