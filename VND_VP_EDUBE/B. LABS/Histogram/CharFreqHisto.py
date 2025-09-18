369080b5bd86b6763f40cedd2d3def9b351b50f1
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-18 11:30:42 +0200

    modified:   VND_VP_EDUBE/B. LABS/Histogram/CharFreqHisto.py

--- VND_VP_EDUBE/B. LABS/Histogram/CharFreqHisto.py
+++ VND_VP_EDUBE/B. LABS/Histogram/CharFreqHisto.py
@@ -21,35 +21,22 @@
 # except IOError as e:
 #     print("I/O error occurred: ", strerror(e.errno))
 ##
+
+## WHEN ENTERING SRCNAME AND DESTINATION NAME, BEST WAY TO MANUALLY INPUT DATA IS TO ADD DIRECT LOCATION
 from os import strerror
 
 srcname = input("Enter the source file name: ")
-try:
-    src = open(srcname, 'rb')
-except IOError as e:
-    print("Cannot open the source file: ", strerror(e.errno))
-    exit(e.errno)	
-
 dstname = input("Enter the destination file name: ")
-try:
-    dst = open(dstname, 'wb')
-except Exception as e:
-    print("Cannot create the destination file: ", strerror(e.errno))
-    src.close()
-    exit(e.errno)	
 
-buffer = bytearray(65536)
-total  = 0
 try:
-    readin = src.readinto(buffer)
-    while readin > 0:
-        written = dst.write(buffer[:readin])
-        total += written
+    with open(srcname, 'rb') as src, open(dstname, 'wb') as dst:
+        buffer = bytearray(65536)
+        total = 0
         readin = src.readinto(buffer)
+        while readin > 0:
+            written = dst.write(buffer[:readin])
+            total += written
+            readin = src.readinto(buffer)
+    print(total, 'byte(s) successfully written')
 except IOError as e:
-    print("Cannot create the destination file: ", strerror(e.errno))
-    exit(e.errno)	
-    
-print(total,'byte(s) succesfully written')
-src.close()
-dst.close()
+    print("I/O error occurred:", strerror(e.errno))
