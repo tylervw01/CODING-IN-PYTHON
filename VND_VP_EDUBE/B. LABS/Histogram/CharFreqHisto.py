cc7ee290bc3f8ce7e2a63cdbc0a35a90f39e74b0
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-18 11:32:24 +0200

    modified:   VND_VP_EDUBE/B. LABS/Histogram/CharFreqHisto.py

--- VND_VP_EDUBE/B. LABS/Histogram/CharFreqHisto.py
+++ VND_VP_EDUBE/B. LABS/Histogram/CharFreqHisto.py
@@ -3,7 +3,7 @@ from os import strerror
 # Initialize 26 counters for each Latin letter.
 # Note: we've used comprehension to do that.
 counters = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 1)}
-file_name = input("Enter the name of the file to analyze: ")
+file_name = input("Enter the name of the file to analyze: ") # ENTER FILE LOCATION
 try:
     f = open(file_name, "rt")
     for line in f:
