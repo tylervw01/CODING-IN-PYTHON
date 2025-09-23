f1aff35e3f1b0a9c0978683d81a939206214b12b
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-09-23 09:05:31 +0200

    new.f

---
+++ VND_VP_EDUBE/B. LABS/DateTime/dateTime.py
@@ -0,0 +1,11 @@
+# from datetime import datetime
+
+# my_date = datetime(2020, 11, 4, 14, 53)
+
+# print(my_date.strftime("%Y/%m/%d %H:%M:%S"))
+# print(my_date.strftime("%y/%B/%d %H:%M:%S %p"))
+# print(my_date.strftime("%a, %Y %b %d"))
+# print(my_date.strftime("%A, %Y %B %d"))
+# print(my_date.strftime("Weekday: %w"))
+# print(my_date.strftime("Day of the year: %j"))
+# print(my_date.strftime("Week number of the year: %W"))
